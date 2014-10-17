dd16540e20dae0cff9501a09f5122d52530fee92
Patrick Hall <pjh@umail.ucsb.edu>
2014-10-17 12:14:32 -0700

    innovative cow vowel counting achievement unlocked

---
+++ js/cowCountViz2000.js
@@ -0,0 +1,36 @@
+function countVowels(text){
+   var letterArray = text.split('');
+   var count = 0;
+   var vowels = ['a','e','i','o','u'];
+
+   letterArray.forEach(function(letter){
+     if(vowels.indexOf(letter) > -1){
+        count += 1
+     }
+   })
+   return count;
+}
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+var button = document.querySelector('button');
+
+button.addEventListener('click', function(ev){
+
+   
+   var text = document.body.querySelector('input').value;
+   var mycount = countVowels(text);
+   for(var i=0; i< mycount; i++){
+     addCow();
+   }
+})   
+
+function addCow(){
+  var protocow = document.querySelector('img');
+  var newCow = protocow.cloneNode(true);
+  document.body.appendChild(newCow);
+}
+
+
+})
