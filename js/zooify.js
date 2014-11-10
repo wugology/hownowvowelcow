f3c93be23c3aac280bfb41de3a111f9bf751eacc
wugology <onna.nelson@gmail.com>
2014-11-10 08:58:55 -0800

    Bug fixes
    Fixes an issue with capital letters not showing.
    Make each new sentence start fresh, rather than appending zoo animals
    forever.

--- js/zooify.js
+++ js/zooify.js
@@ -6,14 +6,16 @@ button.addEventListener('click', function(ev){
    var text = document.body.querySelector('input').value;
    var text2 = text.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~() ]/g,"");
    var text3 = text2.replace(/\d+/g, '');
-      console.log(text3);
-   var letterArray = text3.split('');
+   var text4 = text3.toLowerCase();
+   var letterArray = text4.split('');
+   var field = document.querySelector('#field')
+   field.innerHTML = '';
    for(var i=0; i< letterArray.length; i++){
      var newZoo = "/img/" + letterArray[i] + ".svg";
      console.log(newZoo);
      var ZooImg=document.createElement("img");
      ZooImg.setAttribute('src', newZoo);
-     document.body.appendChild(ZooImg);
+     field.appendChild(ZooImg);
    }
 })   
 
